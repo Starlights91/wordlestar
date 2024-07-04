95a727f75f2f9a190d271e94f441460c447fdb51
0123Mina <ohmina0123@naver.com>
2024-07-05 05:02:07 +0900

    [워들] JavaScript 로직 완성
    간단한 워들 게임을 만들었습니다!

---
+++ js/index.js
@@ -0,0 +1,97 @@
+const 정답 = "APPLE";
+
+//let은 수정이 가능한 변수 (0번째 시도에 1번째 인덱스)
+let attempts = 0;
+let index = 0;
+let timer;
+
+// keydown event (키보드 누름에 따라 키 인식 로직)
+function appStart() {
+  const displayGameover = () => {
+    const div = document.createElement("div");
+    div.innerText = "게임이 종료됐습니다.";
+    div.style =
+      "display: flex; flex-direction: column; justify-content:center; position: absolute; align-items:center; top:60vh; left:35vw; font-size: 20px; background-color: grey; width:200; height:100px; margin: -200px 0 0 0;"; //글자가 정중앙에 위치하도록 해보기
+    document.body.appendChild(div);
+  };
+  const gameover = () => {
+    window.removeEventListener("keydown", WordleKeydown);
+    displayGameover();
+    clearInterval(timer);
+  };
+  const nextLine = () => {
+    if (attempts === 6) return gameover();
+    attempts += 1;
+    index = 0;
+  };
+
+  const handleEnterKey = () => {
+    let 맞은_갯수 = 0;
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-column[data-index='${attempts}${i}']`
+      );
+      const 입력한_글자 = block.innerText;
+      const 정답_글자 = 정답[i];
+      if (입력한_글자 === 정답_글자) {
+        맞은_갯수 += 1;
+        block.style.background = "#6AAA64";
+      } else if (정답.includes(입력한_글자)) block.style.background = "#C9B458";
+      else block.style.background = "#787C7E";
+
+      block.style.color = "white";
+    }
+
+    if (맞은_갯수 === 5) gameover();
+    else nextLine();
+  };
+
+  const handleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-column[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
+    if (index !== 0) index -= 1;
+  };
+
+  //(0번째 시도에 n번째 인덱스)쓰기 위해 backtick ``사용.
+  const WordleKeydown = (event) => {
+    const key = event.key.toUpperCase();
+    const keyCode = event.keyCode;
+    const thisBlock = document.querySelector(
+      `.board-column[data-index='${attempts}${index}']`
+    );
+
+    if (event.key === "Backspace") handleBackspace();
+    // index가 5일 때 즉, 맨 끝까지 단어가 입력이 됐을 때 Enter키를 누르면 handleEnterKey에 대한 동작이 일어나고, Enter키가 아닌 다른 키가 눌리면 그냥 return해서 끝내라는 뜻.
+    else if (index === 5) {
+      if (event.key === "Enter") handleEnterKey();
+      else return;
+    } else if (65 <= keyCode && keyCode <= 90) {
+      thisBlock.innerText = key;
+      index += 1;
+    }
+  };
+  const startTimer = () => {
+    // 타이머
+    const 시작_시간 = new Date();
+
+    function setTime() {
+      const 현재_시간 = new Date();
+      const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+      const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+      const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${분}:${초}`;
+    }
+
+    timer = setInterval(setTime, 1000);
+  };
+
+  startTimer();
+  window.addEventListener("keydown", WordleKeydown);
+}
+
+appStart();
